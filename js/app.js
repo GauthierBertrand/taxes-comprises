c59b913d64e81ac994e5055e8c2cc6a163166e64
GauthierBertrand <gauthierb59@gmail.com>
2023-06-15 14:09:36 +0200

    currency converter added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
 const App = {
+
+  endpoint: "https://api.freecurrencyapi.com/v1/latest?",
+  key: "apikey=icT1TtWAyeXuUdbsCdLHWjqOwTEwjp3mxFw1m94y",
+  currencies: "&currencies=EUR%2CUSD%2CGBP%2CJPY%2CCNY",
+  base: "&base_currency=CAD",
+
   /* Méthode init de l'app */
   init: function () {
-    TaxesCalculator.init();
+
+    fetch(App.endpoint + App.key + App.currencies + App.base)
+      .then((response) => {
+        return response.json();
+      })
+      .then((currencies) => {
+        let data = currencies.data;
+        TaxesCalculator.init(data);
+      })
+      .catch((e) => {
+        alert(
+          "Un problème est survenu avec la récupération des taux de change"
+        );
+      });
   },
 };
+
 //Start the script after the page was entirely loaded
 document.addEventListener("DOMContentLoaded", App.init);
--- js/taxesCalculator.js
+++ js/taxesCalculator.js
@@ -1,24 +1,34 @@
 const TaxesCalculator = {
+  data: null,
+  currency: null,
+  rate: null,
+  ttc: null,
+
   /**
    * Calcul of the price with taxes and display result
    * @param {*} event
    */
   handleCalc: function (event) {
+
     let montantHt = document.getElementById("montantHt");
 
-    let ttc =
+    TaxesCalculator.ttc =
       Math.round(
         ((parseFloat(montantHt.value) * parseFloat(event.target.value)) / 100 +
           parseFloat(montantHt.value)) *
           100
       ) / 100;
-    console.log(ttc);
 
     //retrive the parent display result div
     let parentDiv = document.getElementById("radios");
 
     // display result div creation
     let divElement = document.createElement("div");
+    let conversion = document.createElement("button");
+    conversion.setAttribute("class", "taxe-select btn btn-light");
+    conversion.setAttribute("id", "conversionButton");
+    conversion.addEventListener("click", TaxesCalculator.HandleConverter);
+    conversion.innerHTML = "conversion " + TaxesCalculator.currency;
 
     //id set for display result div
     divElement.setAttribute("id", "result");
@@ -32,23 +42,75 @@ const TaxesCalculator = {
     }
 
     // if ttc not exist place a message else place result
-    if (!ttc) {
+    if (!TaxesCalculator.ttc) {
       divElement.textContent =
         "Veuillez entrer un montant hors taxes puis sélectionner une région.";
     } else {
-      divElement.textContent = "Total = " + ttc + " $";
+      divElement.textContent = "Total = " + TaxesCalculator.ttc + " $";
     }
 
     //place the display result div in dom
+    divElement.append(conversion);
     parentDiv.before(divElement);
   },
 
-  init: function () {
+  /**
+   *Set currency on converter button
+   * @param {*} event
+   */
+  handleGetCurrency: function (event) {
+
+    let button = document.getElementById("conversionButton");
+
+    TaxesCalculator.currency = event.currentTarget.value;
+
+    if (button) {
+      button.innerHTML = "conversion " + TaxesCalculator.currency;
+    }
+  },
+
+  /**
+   * Calcul of amount in the selected currency
+   * @param {*} event
+   */
+  HandleConverter: function (event) {
+
+    rate = TaxesCalculator.data;
+
+    //TaxesCalculator.currency is the key to retrive rate in data
+    result =
+      Math.round(
+        parseFloat(TaxesCalculator.ttc) *
+          parseFloat(rate[TaxesCalculator.currency]) *
+          100
+      ) / 100;
+
+    //Set the result in the button
+    button = event.currentTarget;
+    if (result === NaN) {
+      button.innerHTML = "conversion " + TaxesCalculator.currency;
+    }
+    if (!result) {
+      button.innerHTML = "conversion " + TaxesCalculator.currency;
+    } else {
+      button.innerHTML = result + " " + TaxesCalculator.currency;
+    }
+  },
+
+  init: function (data) {
+    
+    //Set properties on initialization
+    TaxesCalculator.data = data;
+    TaxesCalculator.rate = TaxesCalculator.data[TaxesCalculator.currency];
+    TaxesCalculator.currency = "EUR";
+
     //place an eventLisner on every radio button with a loop
     test = document.getElementsByClassName("taxe-select");
 
     for (let i = 0; i < test.length; i++) {
       test[i].addEventListener("click", TaxesCalculator.handleCalc);
     }
+    let currencies = document.getElementById("currencies");
+    currencies.addEventListener("change", TaxesCalculator.handleGetCurrency);
   },
 };
